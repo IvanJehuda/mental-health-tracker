6ef983680699093a3735c3df18a6f8689ceb5a6e
IvanJehuda <ivanjangi@gmail.com>
2024-11-13 17:09:00 +0700

    Done working Tutorial 8

--- main/views.py
+++ main/views.py
@@ -1,32 +1,30 @@
-from django.shortcuts import render
-from django.shortcuts import render, redirect   # Tambahkan import redirect di baris ini
+import datetime
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.shortcuts import render, redirect, reverse
 from main.forms import MoodEntryForm
 from main.models import MoodEntry
 from django.http import HttpResponse
 from django.core import serializers
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import authenticate, login
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
-import datetime
-from django.http import HttpResponseRedirect
-from django.urls import reverse
-from django.shortcuts import reverse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 from django.utils.html import strip_tags
+import json
+from django.http import JsonResponse
 
-
-# Create your views here.
 @login_required(login_url='/login')
 def show_main(request):
     context = {
         'name': request.user.username,
-        'class': 'PBP D',
-        'npm': '2306123456',
-        'last_login': request.COOKIES['last_login'],
+        'class': 'PBP E',
+        'npm': '2306228756',
+        'last_login': request.COOKIES.get('last_login')
     }
 
     return render(request, "main.html", context)
@@ -54,6 +52,7 @@ def show_json(request):
 def show_xml_by_id(request, id):
     data = MoodEntry.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
 def show_json_by_id(request, id):
     data = MoodEntry.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
@@ -71,24 +70,22 @@ def register(request):
     return render(request, 'register.html', context)
 
 def login_user(request):
-   if request.method == 'POST':
-      form = AuthenticationForm(data=request.POST)
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
 
-      if form.is_valid():
+        if form.is_valid():
             user = form.get_user()
             login(request, user)
             response = HttpResponseRedirect(reverse("main:show_main"))
             response.set_cookie('last_login', str(datetime.datetime.now()))
             return response
-      else:
+        else:
             messages.error(request, "Invalid username or password. Please try again.")
 
-
-
-   else:
-      form = AuthenticationForm(request)
-   context = {'form': form}
-   return render(request, 'login.html', context)
+    else:
+        form = AuthenticationForm(request)
+    context = {'form': form}
+    return render(request, 'login.html', context)
 
 def logout_user(request):
     logout(request)
@@ -112,19 +109,15 @@ def edit_mood(request, id):
     return render(request, "edit_mood.html", context)
 
 def delete_mood(request, id):
-    # Get mood berdasarkan id
     mood = MoodEntry.objects.get(pk = id)
-    # Hapus mood
     mood.delete()
-    # Kembali ke halaman awal
     return HttpResponseRedirect(reverse('main:show_main'))
-
 
 @csrf_exempt
 @require_POST
 def add_mood_entry_ajax(request):
     mood = strip_tags(request.POST.get("mood")) # strip HTML tags!
-    feelings = strip_tags(request.POST.get("feelings"))
+    feelings = strip_tags(request.POST.get("feelings")) # strip HTML tags!
     mood_intensity = request.POST.get("mood_intensity")
     user = request.user
 
@@ -136,4 +129,21 @@ def add_mood_entry_ajax(request):
     new_mood.save()
 
     return HttpResponse(b"CREATED", status=201)
-    ...
+
+@csrf_exempt
+def create_mood_flutter(request):
+    if request.method == 'POST':
+
+        data = json.loads(request.body)
+        new_mood = MoodEntry.objects.create(
+            user=request.user,
+            mood=data["mood"],
+            mood_intensity=int(data["mood_intensity"]),
+            feelings=data["feelings"]
+        )
+
+        new_mood.save()
+
+        return JsonResponse({"status": "success"}, status=200)
+    else:
+        return JsonResponse({"status": "error"}, status=401)
